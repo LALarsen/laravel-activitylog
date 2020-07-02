22f91fd7a0e7ba72f4cf0a0ddc7441c2f6c4f267
Anders Petersen <anders@larsen.no>
2020-07-02 10:55:05 +0200

    Update DetectsChanges.php

--- src/Traits/DetectsChanges.php
+++ src/Traits/DetectsChanges.php
@@ -109,16 +109,9 @@ trait DetectsChanges
         }
 
         if ($this->shouldLogOnlyDirty() && isset($properties['old'])) {
-            $properties['attributes'] = array_udiff_assoc(
+            $properties['attributes'] = $this->array_diff_assoc_recursive(
                 $properties['attributes'],
-                $properties['old'],
-                function ($new, $old) {
-                    if ($old === null || $new === null) {
-                        return $new === $old ? 0 : 1;
-                    }
-
-                    return $new <=> $old;
-                }
+                $properties['old']
             );
             $properties['old'] = collect($properties['old'])
                 ->only(array_keys($properties['attributes']))
@@ -196,5 +189,25 @@ trait DetectsChanges
         $modelAttribute = collect($model->getAttribute($modelAttribute));
 
         return data_get($modelAttribute, implode('.', $path));
+    }
+    private function array_diff_assoc_recursive($array1, $array2): array
+    {
+        $difference = [];
+        foreach ($array1 as $key => $value) {
+            if (is_array($value)) {
+                if (! array_key_exists($key, $array2) || ! is_array($array2[$key])) {
+                    $difference[$key] = $value;
+                } else {
+                    $new_diff = $this->array_diff_assoc_recursive($value, $array2[$key]);
+                    if (! empty($new_diff)) {
+                        $difference[$key] = $new_diff;
+                    }
+                }
+            } elseif (! array_key_exists($key, $array2) || $array2[$key] !== $value) {
+                $difference[$key] = $value;
+            }
+        }
+
+        return $difference;
     }
 }
